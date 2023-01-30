f5bd1e1e23597500e33e101782cfbb1704962303
ndomvi <ndomvi@users.noreply.github.com>
2023-01-30 16:02:57 +0000

    fix ingredient search script

--- javascript.js
+++ javascript.js
@@ -1,19 +1,12 @@
-async function getRecipes(){
-    
-    
-    let searchTerm = document.getElementById("ingInput").value;
-    
-    //read json file and parse
-    fs.readFileSync();
-    const response = await fetch('ingredients.json');
-    const ingData = await response.json();
-    ingData = JSON.parse(ingData);
-    
+async function getRecipes() {
+  let searchTerm = document.getElementById('ingInput').value
 
-    const searchResults = ingData.filter(data => {return data.ingredients.name.includes(searchTerm);});
+  //read json file and parse
+  const response = await fetch('ingredients.json')
+  const ingData = await response.json()
 
-    //testing if correct ingredient is found
-    document.getElementById("output").innerHTML = searchResults.ingredients.id;
-    
+  const searchResults = ingData.data.ingredients.filter((ingredient) => ingredient.name.includes(searchTerm))
+
+  //testing if correct ingredient is found
+  document.getElementById('output').innerHTML = searchResults.map((ing) => `<div>${ing.id}: ${ing.name}</div>`)
 }
-
