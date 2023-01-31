f910c9c8b3833cfa60c8559826f05877e3d8c9bc
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2023-01-31 18:16:19 +0000

    Testing recipe search with ingredient input.

--- javascript.js
+++ javascript.js
@@ -7,6 +7,34 @@ async function getRecipes() {
 
   const searchResults = ingData.data.ingredients.filter((ingredient) => ingredient.name.includes(searchTerm))
 
-  //testing if correct ingredient is found
-  document.getElementById('output').innerHTML = searchResults.map((ing) => `<div>${ing.id}: ${ing.name}</div>`)
+  response = await fetch('recipes.json')
+  const recData = await response.json()
+
+  const ingID = searchResults[0].id
+
+  const collectedRecipes = recData.data.recipes.filter((recipe) => recipe.ingredients.find((element) => element.id.includes(ingID)))
+
+  collectedRecipes.forEach(displayRecipes)
+
+}
+
+function displayRecipes(item) {
+
+  const container = document.getElementById('output')
+  
+  
+  
+  // Construct card content
+  const content = `
+  <div class="col">
+    <div class="card" style=" border-radius: 7px;">
+      <div class="card-body shadow">
+      <h5 class="card-title  text-center color-text-brownish"><b>${item.name}</b></h5>
+      </div>
+      <img src="https://picsum.photos/300/200" class="card-img-top p-1" style="border-radius: 10px;" alt="..." height="200px">
+    </div>
+  </div>
+  `
+
+  container.innerHTML += content
 }
