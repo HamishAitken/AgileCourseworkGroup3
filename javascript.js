af7cc2b8cb399e101b7d23f37d9b94ab9a9d7fcd
ndomvi <ndomvi@users.noreply.github.com>
2023-01-31 18:56:24 +0000

    fix recipe search by ingredient

--- javascript.js
+++ javascript.js
@@ -1,40 +1,37 @@
 async function getRecipes() {
-  let searchTerm = document.getElementById('ingInput').value
+  let searchTerm = document.getElementById('formIngredients').value
 
   //read json file and parse
-  const response = await fetch('ingredients.json')
-  const ingData = await response.json()
+  // TODO: handle missing file
+  const ingData = await fetch('ingredients.json').then((res) => res.json())
 
   const searchResults = ingData.data.ingredients.filter((ingredient) => ingredient.name.includes(searchTerm))
 
-  response = await fetch('recipes.json')
-  const recData = await response.json()
+  // TODO: handle missing file
+  const recData = await fetch('recipes.json').then((res) => res.json())
 
+  // TODO: dropdown to allow user to select the ingredient (instead of using the first one)
+  // TODO: handle not finding any ingredients
   const ingID = searchResults[0].id
 
-  const collectedRecipes = recData.data.recipes.filter((recipe) => recipe.ingredients.find((element) => element.id.includes(ingID)))
+  // TODO: handle not finding any recipes (show a message?)
+  const collectedRecipes = recData.data.recipes.filter((recipe) => recipe.ingredients.find((ing) => ing.id === ingID))
 
-  collectedRecipes.forEach(displayRecipes)
+  const recipeContainer = document.getElementById('recipeList')
+  recipeContainer.innerHTML = ''
 
-}
-
-function displayRecipes(item) {
-
-  const container = document.getElementById('output')
-  
-  
-  
-  // Construct card content
-  const content = `
-  <div class="col">
-    <div class="card" style=" border-radius: 7px;">
-      <div class="card-body shadow">
-      <h5 class="card-title  text-center color-text-brownish"><b>${item.name}</b></h5>
+  collectedRecipes.forEach((recipe) => {
+    const recipeCard = `
+    <div class="col">
+      <div class="card" style=" border-radius: 7px;">
+        <div class="card-body shadow">
+        <h5 class="card-title  text-center color-text-brownish"><b>${recipe.name}</b></h5>
+        </div>
+        <img src="https://picsum.photos/300/200" class="card-img-top p-1" style="border-radius: 10px;" alt="..." height="200px">
       </div>
-      <img src="https://picsum.photos/300/200" class="card-img-top p-1" style="border-radius: 10px;" alt="..." height="200px">
     </div>
-  </div>
-  `
+    `
 
-  container.innerHTML += content
+    recipeContainer.innerHTML += recipeCard
+  })
 }
