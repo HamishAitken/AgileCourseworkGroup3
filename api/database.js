2ef0780312ff18895173f37e217e7afa0c9ce799
Jmomanyi <jjjmomanyi@gmail.com>
2023-02-01 19:42:54 +0000

    login working JWT verification remaining

--- api/database.js
+++ api/database.js
@@ -1,14 +1,21 @@
 const lokijs = require('lokijs')
-
+const bcrypt = require("bcrypt")
 // TODO: replace lokijs with something like mongoose, and host the DB on a separate server
 var db = new lokijs('database.db')
 // The database is saved every 5 seconds. Might want to increase that
 db.autosaveEnable()
 db.loadDatabase({}, () => {
-  var recipes = db.getCollection('recipes') || db.addCollection('recipes')
+  var recipes = db.getCollection('recipes') || db.addCollection('recipes');
+  var users = db.getCollection('users') || db.addCollection('users');
   //   recipes.insertOne({ test: 'test' })
   //   db.saveDatabase()
-  console.log(recipes.data)
+  bcrypt.hash("test123", 10).then((hash) => {
+    // users.insertOne({
+    //   username: "admin",
+    //   password: hash
+    // })
+    console.log(users.data)
+  })
 })
 
 module.exports = db
--- api/main.js
+++ api/main.js
@@ -3,8 +3,13 @@ const cors = require('cors')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const adminRouter = require('./routes/admin.js')
+const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
+require('dotenv').config()
 
-const PORT = 3000
+const db = require("./database.js")
+
+const PORT = 3001
 console.log('Starting the server...')
 
 const app = express()
@@ -16,23 +21,57 @@ app.use(
       directives: {
         scriptSrc: ["'self'", "'unsafe-inline'", 'https:'],
         imgSrc: ["'self'", 'data:', 'https:'],
+        upgradeInsecureRequests: null,
       },
     },
     crossOriginEmbedderPolicy: { policy: 'credentialless' },
   })
 )
+
 app.use(
   morgan(':remote-addr - :remote-user [:date[iso]] :method :url :status (:response-time ms) - :res[content-length]')
 )
+
 app.use(express.json())
 
 app.get('/api', (req, res) => {
   res.send('LGL api. Hello there!')
 })
 
+
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body.login;
+
+  const user = db.getCollection('users').findOne((user) => user.username === username)
+  if (!user) {
+    res.status(401).send(JSON.stringify("Invalid login/password"))
+  }
+
+
+  bcrypt.compare(password, user.password).then((result) => {
+    if (!result) {
+      res.status(401).send(JSON.stringify({ error: "Invalid login/password" }))
+    }
+    else {
+      // TODO: generate and send JWT
+      const token = jwt.sign({ username: user.username }, process.env.SECRET, { expiresIn: 60 * 60 * 6 /* 6 hours */})
+      res.json({
+        token,
+        username: user.username,
+
+      })
+
+
+    }
+  })
+
+})
+
+
 app.use('/api/admin', adminRouter)
 
 app.use(express.static('../web'))
+app.use('/assets', express.static('../assets'))
 
 app.listen(PORT, () => {
   console.log('Listening on port:', PORT)
