4e22b36114e76992f62ae2e299612d92ccc45b8d
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2023-02-01 16:45:30 +0000

    Updated recipe message

--- javascript.js
+++ javascript.js
@@ -20,22 +20,25 @@ async function getRecipes() {
   if (collectedRecipes.length == 0 ) {
     recipeContainer.innerHTML = "<p> No recipes match ingredients</p>"
   } 
-
-  const recipeContainer = document.getElementById('recipeList')
-  recipeContainer.innerHTML = ''
-
-  collectedRecipes.forEach((recipe) => {
-    const recipeCard = `
-    <div class="col">
-      <div class="card" style=" border-radius: 7px;">
-        <div class="card-body shadow">
-        <h5 class="card-title  text-center color-text-brownish"><b>${recipe.name}</b></h5>
+  else{
+    const recipeContainer = document.getElementById('recipeList')
+    recipeContainer.innerHTML = ''
+  
+    collectedRecipes.forEach((recipe) => {
+      const recipeCard = `
+      <div class="col">
+        <div class="card" style=" border-radius: 7px;">
+          <div class="card-body shadow">
+          <h5 class="card-title  text-center color-text-brownish"><b>${recipe.name}</b></h5>
+          </div>
+          <img src="https://picsum.photos/300/200" class="card-img-top p-1" style="border-radius: 10px;" alt="..." height="200px">
         </div>
-        <img src="https://picsum.photos/300/200" class="card-img-top p-1" style="border-radius: 10px;" alt="..." height="200px">
       </div>
-    </div>
-    `
+      `
+  
+      recipeContainer.innerHTML += recipeCard
+    })
+  }
 
-    recipeContainer.innerHTML += recipeCard
-  })
+ 
 }
