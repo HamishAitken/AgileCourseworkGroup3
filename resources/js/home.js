5e923a60de2b71b8e51619fb85426eb059270392
Marlon288 <71406913+Marlon288@users.noreply.github.com>
2023-02-01 12:36:24 +0000

    Applied changes proposed
    Small adjustments to the structure and files

---
+++ resources/js/home.js
@@ -0,0 +1,13 @@
+var selectedItems = [];
+
+localStorage.
+
+function selectIngredient(clicked_id){
+    console.log(clicked_id);
+    el = Document.getElementById(clicked_id);
+    
+    if(el.style.color == "#FFFFFF"){
+        el.style.color =
+    }
+
+}
