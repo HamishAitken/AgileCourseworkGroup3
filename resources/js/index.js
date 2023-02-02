7ee3add11725b1f868fdd09ae49717a22ee801e2
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2023-02-02 17:58:11 +0000

    Testing with search

--- resources/js/index.js
+++ resources/js/index.js
@@ -160,28 +160,8 @@ async function getRecipes() {
   
     
   
-    if (collectedRecipes.length == 0 ) {
-      recipeContainer.innerHTML = "<p> No recipes match ingredients</p>"
-    } 
-    else{
-      
-      recipeContainer.innerHTML = ''
-    
-      collectedRecipes.forEach((recipe) => {
-        const recipeCard = `
-        <div class="col">
-          <div class="card" style=" border-radius: 7px;">
-            <div class="card-body shadow">
-            <h5 class="card-title  text-center color-text-brownish"><b>${recipe.name}</b></h5>
-            </div>
-            <img src="https://picsum.photos/300/200" class="card-img-top p-1" style="border-radius: 10px;" alt="..." height="200px">
-          </div>
-        </div>
-        `
-    
-        recipeContainer.innerHTML += recipeCard
-      })
-    }
+    const recipeNames = collectedRecipes.map(recipe => recipe.name)
+    generateRecipes(recipeNames)
   
    
   }
