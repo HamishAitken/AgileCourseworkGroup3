36f082b4444d98160043a4ae860562356a0fc7a0
bijoy-shah <55397445+bijoy-shah@users.noreply.github.com>
2023-02-03 18:44:42 +0000

    Update resources/js/details.js
    Co-authored-by: Dmitrijs <46864422+ndomvi@users.noreply.github.com>

--- resources/js/details.js
+++ resources/js/details.js
@@ -1,5 +1,6 @@
 function addToShoppingCart(element){
     //Get the Text of the <p> Element of the List where the button was clicked.
+    // TODO: save ingredient IDs instead of ingredient names. Use the IDs in the cart to group and add links to similar ingredients.
     let text = element.previousElementSibling.innerText;
     console.log(text);
     var cart = JSON.parse(localStorage.getItem("cart"));
